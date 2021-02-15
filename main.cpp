d9e35bbef39ffb21af92010a5e8d7cc743af6d98
Afifah Najihah Mohd Syukri <jiesyukri12@gmail.com>
2021-02-15 04:48:07 +0000

    create Class Guess

--- main.cpp
+++ main.cpp
@@ -6,11 +6,30 @@
 #include<string>
 using namespace std;
 
+//Hangman has Guess - Composition
+class Guess {
+  private:
+    char guess;
+    string word;
+  
+  public:
+  Guess(){}
+  Guess(string a_word,char a_guess){
+
+    word = a_word;
+    guess = a_guess;
+  }
+
+  void displayWord(){
+
+    cout << word << endl;
+  }
+  
 class Hangman {
 
   private:
   int tries;
-  char guess;
+  Guess *g;
 
   public:
   Hangman(){}
@@ -19,7 +38,17 @@ class Hangman {
   bool CheckGuess ();
 };
 
+};
+
 int main(){
+
+  char guess;
+  cout << "Enter guess: ";
+  cin >> guess;
+  Guess g1("Programming",guess);
+
+  g1.displayWord();
+  
 
   return 1;
   //system("pause");
