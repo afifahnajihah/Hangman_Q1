9b69fe6d35c376b2eed07b8fc653fd286ac913f8
Afifah Najihah Mohd Syukri <jiesyukri12@gmail.com>
2021-02-15 07:18:33 +0000

    debugging checkGuess

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ class Guess {
   private:
     char guess;
     string word = "programming";
+    string m_word; //m_word -> MysteryWord
 
   
   public:
@@ -20,16 +21,40 @@ class Guess {
     word = a_word;
     guess = a_guess;
   }
+  
+  char getGuess() { return guess;}
+  bool findLetter (char guess);
+  
 
-  void displayWord(){
+  void displayWord();
+};
+
+void Guess :: displayWord(){
 
     //cout << word << endl; //for debugging purpose
-    for(int i = 0;i< word[i] ; i++){
-      cout << "-";
+    for(int i = 0;i< word.length() ; i++){
+      m_word[i] = '-';
+      cout << m_word[i];
     }
     cout << endl;
   }
-};
+
+bool Guess :: findLetter(char guess){
+
+    for(int i = 0;i<word.length();i++){
+
+      if (word[i] == guess){
+        m_word[i] = guess;
+        return true;
+        displayWord();
+      }
+
+      else if (word[i] != guess){
+        return false;
+        //displayWord();
+      }
+    }
+}
 
 class Hangman {
 
@@ -51,39 +76,58 @@ void Hangman::startGame(Guess *gs){
 
 }
 
+bool Hangman::CheckGuess (Guess g,int tries){
+
+  char guess = g.getGuess();
+  if(g.findLetter(guess)== true){
+    g.displayWord();
+  } 
+
+  else{
+    g.displayWord();
+    tries--;
+  }
+
+
+
+}
+
 
 int main(){
-  /*cout << "Simple Hangman Game " << endl;
-  cout << "__________   " << endl;
-cout << "|     }      " << endl;
-cout << "|     0      "  << endl;
-cout << "|     |      " << endl;
-cout << "|    /|\\    " << endl;
-cout << "|   / | \\   " << endl;
-cout << "|    / \\    " << endl;
-cout << "|   /   \\   "  << endl;
-cout << "|___________ " << endl;
-*/
 
-//set variables used in this game
-int misses;
-char guess;
-bool CorrectGuess = false;
+    /*cout << "Simple Hangman Game " << endl;
+    cout << "__________   " << endl;
+    cout << "|     }      " << endl;
+    cout << "|     0      "  << endl;
+    cout << "|     |      " << endl;
+    cout << "|    /|\\    " << endl;
+    cout << "|   / | \\   " << endl;
+    cout << "|    / \\    " << endl;
+    cout << "|   /   \\   "  << endl;
+    cout << "|___________ " << endl;
+    */
 
-//create object for Hangman
-Hangman game;
-Guess g;
+    //set variables used in this game
+    int misses = 10;
+    char guess;
+    bool CorrectGuess = false;
 
-//while(misses >= 0){
-  
-  //Display information related to the word
-  cout << "Guess: ";
-  cin >> guess;
-  
-  game.startGame(&g);
-  
+    //create object for Hangman
+    Hangman game;
+    Guess g;
 
-//}
-  return 1;
-  //system("pause");
+    //while(misses >= 0){
+      
+      //Display information related to the word
+      cout << "Guess: ";
+      cin >> guess;
+      
+      game.startGame(&g);
+      game.CheckGuess(g, misses);
+      
+      
+
+    //}
+      return 1;
+      //system("pause");
 }
